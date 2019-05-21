026e9befaf16fde75e8fff7b068cf5563616e495
Samir <samir.fennikh@gmail.com>
2019-05-21 18:08:11 +0200

    editing movie page (button, info)

--- the-movie-app/src/Pages/Movie.jsx
+++ the-movie-app/src/Pages/Movie.jsx
@@ -1,11 +1,12 @@
 import React, { Component, Fragment } from 'react';
 
-// Styled components
+// Packages
 import styled from 'styled-components';
 import axios from "axios";
 
-const BackroundMovie = styled.img`
+const BackgroundMovie = styled.img`
     width: 100%;
+    height: 100%;
     position: absolute;
     filter: blur(5px);
     z-index: 1;
@@ -15,7 +16,7 @@ const Layer = styled.div`
     position: absolute;
     width: 100%;
     height: 100vh;
-    background-color: #020140;
+    background-color: #000;
     opacity: .6;
     z-index: 10000;
 `
@@ -34,14 +35,51 @@ const ContentRight = styled.div`
     display: flex;
     width: 50%;
 `
+
 const Poster = styled.img`
     width: 300px;
+    height: 450px;
     z-index: 10000;
     margin-top: 20%;
-    margin-left: 10%;
+    margin-left: 25%;
     border-radius: 20px;
     box-shadow: 4px 5px 25px 0px rgba(0,0,0,0.75);
 `
+
+const MovieInfo = styled.div`
+    margin-top: 10px;
+`
+
+const MovieTitle = styled.h1`
+    font-weight: bold;
+    color: #fff;
+    margin-top: 25%;
+    margin-left: 25%;
+    font-size: 50px;
+    width: 70%;
+`
+
+const MovieOverview = styled.div`
+    color: #fff;
+    opacity: .7;
+    margin-top: 16px;
+    margin-left: 25%;
+    font-weight: bold;
+    line-height: 30px;
+    width: 90%;
+`
+
+const SeeMore = styled.button`
+    color: #fff;
+    background-color: #008ae1;
+    font-size: 30px;
+    padding: 10px 40px 12px;
+    font-weight: bold;
+    border-radius: 50px;
+    border: none;
+    margin: 30px 25%;
+`;
+
 
 class Movie extends Component {
     constructor(props) {
@@ -52,7 +90,8 @@ class Movie extends Component {
             release: null,
             genre: null,
             rating: null,
-            vote: null
+            vote: null,
+            overview: null
         }
     };
 
@@ -68,7 +107,8 @@ class Movie extends Component {
                     release: res.data.release_date,
                     genre: res.data.genres[0].id,
                     rating: res.data.vote_average,
-                    vote: res.data.vote_count
+                    vote: res.data.vote_count,
+                    overview: res.data.overview
                 });
             });
     }
@@ -76,11 +116,15 @@ class Movie extends Component {
     render() {
         return (
             <Fragment>
-                <BackroundMovie src={"https://image.tmdb.org/t/p/w500_and_h282_face" + this.state.poster} alt={this.state.title} />
+                <BackgroundMovie src={"https://image.tmdb.org/t/p/w500_and_h282_face" + this.state.poster} alt={this.state.title} />
                 <Layer />
                 <ContentContainer>
                     <ContentLeft>
-                        {this.state.title}
+                        <MovieInfo>
+                            <MovieTitle>{this.state.title}</MovieTitle>
+                            <MovieOverview>{this.state.overview}</MovieOverview>
+                            <SeeMore>See more</SeeMore>
+                        </MovieInfo>
                     </ContentLeft>
                     <ContentRight>
                         <Poster src={"https://image.tmdb.org/t/p/w600_and_h900_bestv2" + this.state.poster} />
